9967c5deba57ebebe4adb93b1304cef5b2824c12
edsonphx <68605321+edsonphx@users.noreply.github.com>
2020-12-25 06:10:15 -0300

    Update GetExpressionTree.cs

--- GetExpressionTree.cs
+++ GetExpressionTree.cs
@@ -7,20 +7,23 @@ namespace Core
     public class GetExpressionTree
     {
         private static Dictionary<KeyValuePair<int, Side>, Dictionary<Side, Expression>> _expressionTree = new Dictionary<KeyValuePair<int, Side>, Dictionary<Side, Expression>>();
+        private static List<KeyValuePair<int, Side>> _keys = new List<KeyValuePair<int, Side>>();
         public static void Main(string[] args)
         {
             new GetExpressionTree().Start();
         }
-        
-        private void Start()
+        public void Start()
         {
-            Expression<Func<int, int, int>> exp = (x, y) => (x + y + 1 + x + y + 1) * 10;
+            Expression<Func<int, int, int>> exp = (x, y) => 8 + x + y + 1 + 3 + 1 + 3;
             GenerateExpressionTree(exp.Body, 0);
+            
+            _expressionTree.CountNumberOfVar(_keys);
         }
 
         private void GenerateExpressionTree(Expression exp, int depth = 0, Side side = Side.None)
         {
             var key = CreateKey(depth, side);
+            _keys.Add(key);
 
             if (exp.NodeType == ExpressionType.Constant || exp.NodeType == ExpressionType.Parameter)
             {
@@ -69,5 +72,60 @@ namespace Core
         None = -1,
         Left = 0,
         Right = 1
+    }
+    public static class ExtensionMethods
+    {
+        public static int CountNumberOfVar(this Dictionary<KeyValuePair<int, Side>, Dictionary<Side, Expression>> dict, IEnumerable<KeyValuePair<int, Side>> keys)
+        {
+            int numberOfVar = 0;
+
+            foreach (var key in keys)
+            {
+                if (dict[key].ContainsKey(Side.None))
+                {
+                    numberOfVar += 1;
+                }
+            }
+
+            return numberOfVar;
+        }
+
+        public static int CountNumberOfParameters(this Dictionary<KeyValuePair<int, Side>, Dictionary<Side, Expression>> dict, IEnumerable<KeyValuePair<int, Side>> keys)
+        {
+            int numberOfParameters = 0;
+
+            foreach (var key in keys)
+            {
+                if (dict[key].ContainsKey(Side.None))
+                {
+                    if (dict[key][Side.None].NodeType == ExpressionType.Parameter)
+                    {
+
+                        numberOfParameters += 1;
+                    }
+                }
+            }
+
+            return numberOfParameters;
+        }
+
+        public static int CountNumberOfConstants(this Dictionary<KeyValuePair<int, Side>, Dictionary<Side, Expression>> dict, IEnumerable<KeyValuePair<int, Side>> keys)
+        {
+            int numberOfParameters = 0;
+
+            foreach (var key in keys)
+            {
+                if (dict[key].ContainsKey(Side.None))
+                {
+                    if (dict[key][Side.None].NodeType == ExpressionType.Constant)
+                    {
+
+                        numberOfParameters += 1;
+                    }
+                }
+            }
+
+            return numberOfParameters;
+        }
     }
 }
